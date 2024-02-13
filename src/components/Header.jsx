9abfeef887108f47d720f0b4fff3cf2092e6c964
henry nnamani <nnamani.henry@sevenup.org>
2024-02-13 18:13:52 +0100

    comit first

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    const user = {
+        name: "John",
+    };
+
+    const cart = {
+        totalCount: 10,
+    }
+
+    const logout= ()=> {
+
+    }
+  return (
+    <header className=' header bg-white p-0 border-b border-[#e72929]'>
+        <div className='mx-auto flex justify-between'>
+            <Link to={"/"} className='font-bold p-[1rem]' >
+                Amailo Food!
+            </Link>
+
+            <nav>
+                <ul>
+                    {
+                        user ? (
+                            <li className='relative menu_container'>
+                                <Link to="/profile">{user.name}</Link>
+                                <div className='absolute z-[1] bg-[whitesmoke] hidden menu'>
+                                    <Link to="/profile">Profile</Link>
+                                    <Link to="/orders">Orders</Link>
+                                    <a className='' onClick={logout}>Logout</a>
+                                </div>
+                            </li>
+                        ) : (
+                            <Link to={"/login"}></Link>
+                        )
+                    }
+
+                    <li>
+                        <Link to={"/cart"}>
+                        Cart
+                        {cart.totalCount > 0 && <span className='cart_count'>{cart.totalCount}</span>}
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
